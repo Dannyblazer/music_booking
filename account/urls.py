c13b627634eca8d29f97548917f5c9d220d4d127
Dannyblazer <nnajidanny004@gmail.com>
2025-03-31 19:52:26 +0100

    refactored route

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ArtistProfileViewSet, UserRegisterView
+from .views import ArtistProfileViewSet, UserRegisterView, UserLoginView
 
 router = DefaultRouter()
 router.register(r'artists', ArtistProfileViewSet)
@@ -8,4 +8,5 @@ router.register(r'artists', ArtistProfileViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/register/', UserRegisterView.as_view(), name='register'),
+    path('auth/login/', UserLoginView.as_view(), name='login'),
 ]
